a092b1b2e1d64ff3bc8a480a42582b7436046583
L-proger <l-proger@yandex.ru>
2020-02-23 02:02:26 +0300

    Fix bug in USB device enumeration

--- Src/LFramework/USB/Host/UsbHostManager.h
+++ Src/LFramework/USB/Host/UsbHostManager.h
@@ -34,47 +34,47 @@ public:
     }
 
     static std::vector<UsbHostDeviceInfo> enumerateDevices(){
-        std::vector<UsbHostDeviceInfo> result;
-        GUID usbClassGUID = { 0xA5DCBF10, 0x6530, 0x11D2, {0x90, 0x1F, 0x00, 0xC0, 0x4F, 0xB9, 0x51, 0xED} };
+		std::vector<UsbHostDeviceInfo> result;
+		GUID usbClassGUID = { 0xA5DCBF10, 0x6530, 0x11D2, {0x90, 0x1F, 0x00, 0xC0, 0x4F, 0xB9, 0x51, 0xED} };
 
-        //Get device list
-        auto deviceList = SetupDiGetClassDevsA(&usbClassGUID, nullptr, nullptr, DIGCF_DEVICEINTERFACE | DIGCF_PRESENT);
-        if (deviceList == INVALID_HANDLE_VALUE) {
-            return result;
-        }
+		//Get device list
+		auto deviceList = SetupDiGetClassDevsA(&usbClassGUID, nullptr, nullptr, DIGCF_DEVICEINTERFACE | DIGCF_PRESENT);
+		if (deviceList == INVALID_HANDLE_VALUE) {
+			return result;
+		}
 
-        //Iterate devices
-        for(DWORD i = 0; GetLastError() != ERROR_NO_MORE_ITEMS; ++i){
-            //Get device interface
-            SP_DEVICE_INTERFACE_DATA deviceInterfaceData{};
-            deviceInterfaceData.cbSize = sizeof(SP_DEVICE_INTERFACE_DATA);
-            if(SetupDiEnumDeviceInterfaces(deviceList, nullptr, &usbClassGUID, i, &deviceInterfaceData) == FALSE){
-                if( GetLastError() != ERROR_NO_MORE_ITEMS){
-                    continue;
-                }else{
-                    break;
-                }
-            }
+		//Iterate devices
+		for (DWORD i = 0; GetLastError() != ERROR_NO_MORE_ITEMS; ++i) {
+			//Get device interface
+			SP_DEVICE_INTERFACE_DATA deviceInterfaceData{};
+			deviceInterfaceData.cbSize = sizeof(SP_DEVICE_INTERFACE_DATA);
+			if (SetupDiEnumDeviceInterfaces(deviceList, nullptr, &usbClassGUID, i, &deviceInterfaceData) == FALSE) {
+				if (GetLastError() != ERROR_NO_MORE_ITEMS) {
+					continue;
+				} else {
+					break;
+				}
+			}
 
-            //Get device interface details
-            DWORD deviceInterfaceDataSize = 0;
-            SetupDiGetDeviceInterfaceDetailA(deviceList, &deviceInterfaceData, nullptr, 0, &deviceInterfaceDataSize, nullptr);
+			//Get device interface details
+			DWORD deviceInterfaceDataSize = 0;
+			SetupDiGetDeviceInterfaceDetailA(deviceList, &deviceInterfaceData, nullptr, 0, &deviceInterfaceDataSize, nullptr);
 
-            std::vector<std::uint8_t> deviceInterfaceDetailBuffer;
-            deviceInterfaceDetailBuffer.resize(deviceInterfaceDataSize, 0);
+			std::vector<std::uint8_t> deviceInterfaceDetailBuffer;
+			deviceInterfaceDetailBuffer.resize(deviceInterfaceDataSize, 0);
 
-            auto deviceInterfaceDetailData = reinterpret_cast<PSP_DEVICE_INTERFACE_DETAIL_DATA_A>(deviceInterfaceDetailBuffer.data());
-            deviceInterfaceDetailData->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA);
+			auto deviceInterfaceDetailData = reinterpret_cast<PSP_DEVICE_INTERFACE_DETAIL_DATA_A>(deviceInterfaceDetailBuffer.data());
+			deviceInterfaceDetailData->cbSize = sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA_A);
 
-            SP_DEVINFO_DATA deviceData{};
-            deviceData.cbSize = sizeof(deviceData);
+			SP_DEVINFO_DATA deviceData{};
+			deviceData.cbSize = sizeof(deviceData);
 
-            if (SetupDiGetDeviceInterfaceDetailA(deviceList, &deviceInterfaceData, deviceInterfaceDetailData, deviceInterfaceDataSize, &deviceInterfaceDataSize, &deviceData) == TRUE){
-                auto info = getUsbDeviceInfo(std::string(deviceInterfaceDetailData->DevicePath));
-                result.push_back(info);
-            }
-        }
-        SetupDiDestroyDeviceInfoList(deviceList);
-        return result;
+			if (SetupDiGetDeviceInterfaceDetailA(deviceList, &deviceInterfaceData, deviceInterfaceDetailData, deviceInterfaceDataSize, &deviceInterfaceDataSize, &deviceData) == TRUE) {
+				auto info = getUsbDeviceInfo(std::string(deviceInterfaceDetailData->DevicePath));
+				result.push_back(info);
+			}
+		}
+		SetupDiDestroyDeviceInfoList(deviceList);
+		return result;
     }
 };
