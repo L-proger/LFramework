5c9be3d439173d52bf92e0675fe956ba3059d884
l-proger <l-proger@yandex.ru>
2022-11-04 00:18:41 +0300

    *

--- Src/LFramework/IO/StreamWriter.cpp
+++ Src/LFramework/IO/StreamWriter.cpp
@@ -5,7 +5,7 @@ namespace LFramework {
 OutputStream<char>& operator <<(OutputStream<char>& stream, const char* value){
 	auto len = strlen(value);
 	OutputStreamFormatter::fieldBegin(stream, len);
-	stream.write(value, len);
+	stream.write(value, static_cast<int>(len));
 	OutputStreamFormatter::fieldEnd(stream, len);
 	return stream;
 }
